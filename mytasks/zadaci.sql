e0aa15c359d8077c3c73896b8cfdbda890bffc31
Ante Gulin <theguxi@gmail.com>
2016-03-11 17:22:26 +0100

    Removed unnecessary files
    Removed files that aren't needed

--- mytasks/zadaci.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `zadaci` (
-  `title` varchar(32) NOT NULL,
-  `description` text NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `started` datetime NOT NULL,
-  `finished` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
